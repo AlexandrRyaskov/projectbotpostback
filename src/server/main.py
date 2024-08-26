899e1d62ac336856eaae636994785daf64d65c6f
Alexandr Ryaskov <blantyk@mail.ru>
2024-08-26 21:55:11 +0500

    /Users/admin/Pycharm/Projects/telegrambot/.idea

--- src/server/main.py
+++ src/server/main.py
@@ -8,7 +8,7 @@ from bot.user_status import UserStatus
 app = FastAPI()
 
 
-@app.get("/user/registered/{user_id}")
+@app.get("/user/registered/{win_user_id}")
 async def read_item(tg_user_id: str, password: str, win_user_id: str):
     if password != config.postback_password.get_secret_value():
         return
@@ -25,7 +25,7 @@ async def read_item(tg_user_id: str, password: str, win_user_id: str):
         )
 
 
-@app.get("/user/made_deposit/{user_id}")
+@app.get("/user/made_deposit/{win_user_id}")
 async def read_item(win_user_id: str, password: str):
     if password != config.postback_password.get_secret_value():
         return
@@ -41,8 +41,8 @@ async def read_item(win_user_id: str, password: str):
         )
 
 
-# http://194.59.247.173:8080/user/registered/{user_id}/?{sub1}=tg_user_id?password=dsjfbsdlkhfldshfhwdsfgjsdjf2189372189743124675136$$$newsflash
-# http://194.59.247.173:8080/user/made_deposit/{user_id}?password=dsjfbsdlkhfldshfhwdsfgjsdjf2189372189743124675136$$$newsflash
+# http://194.59.247.173:8080/user/registered/{user_id}/?tg_user_id={sub1}
+# http://194.59.247.173:8080/user/made_deposit/{user_id}
 
 # http://194.59.247.173:8080/user/registered/{user_id}/?tg_user_id={sub1}?password=dsjfbsdlkhfldshfhwdsfgjsdjf2189372189743124675136$$$newsflash
 # http://194.59.247.173:8080/user/made_deposit/{user_id}?password=dsjfbsdlkhfldshfhwdsfgjsdjf2189372189743124675136$$$newsflash
